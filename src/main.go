0203242e6fb9e173644e15dc9db53c5f75db50e7
zhangbingbing <tosft.sohu.com>
2016-10-12 10:13:05 +0800

    add read file func

--- src/main.go
+++ src/main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"strings"
 	"utils"
 )
+
+const success = "success"
 
 func main() {
 	flag.Parse()
 	root := flag.Arg(0)
-	util.GetFileList(root)
+	fileNames := utils.GetFileList(root)
+	for _, v := range fileNames {
+		fmt.Println(v)
+		lines := utils.ReadFile(v)
+		num := sliceContainsSuccess(lines)
+		fmt.Println(num)
+	}
+}
+
+func sliceContainsSuccess(strs []string) int32 {
+	var num int32 = 0
+	for _, v := range strs {
+		if strContainSuccess(v) {
+			num++
+		}
+	}
+	return num
+}
+
+func strContainSuccess(str string) bool {
+	return strings.Contains(str, success)
 }
--- src/utils/fileUtil.go
+++ src/utils/fileUtil.go
@@ -1,7 +1,9 @@
-package util
+package utils
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
@@ -9,7 +11,8 @@ import (
 /**
 获取路径下的所有文件
 */
-func GetFileList(path string) {
+func GetFileList(path string) []string {
+	var fileNames []string
 	err := filepath.Walk(path, func(path string, f os.FileInfo, err error) error {
 		if f == nil {
 			return err
@@ -17,10 +20,28 @@ func GetFileList(path string) {
 		if f.IsDir() {
 			return nil
 		}
-		println(path)
+		fileNames = append(fileNames, path)
 		return nil
 	})
 	if err != nil {
 		fmt.Printf("filepath.Walk() returned %v\n", err)
 	}
+	return fileNames
+}
+
+func ReadFile(fileName string) []string {
+	var lines []string
+	f, err := os.Open(fileName) //打开文件
+	defer f.Close()             //打开文件出错处理
+	if nil == err {
+		buff := bufio.NewReader(f) //读入缓存
+		for {
+			line, err := buff.ReadString('\n') //以'\n'为结束符读入一行
+			if err != nil || io.EOF == err {
+				break
+			}
+			lines = append(lines, line)
+		}
+	}
+	return lines
 }
