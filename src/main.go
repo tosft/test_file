53a302ca55c43c6ba9298fba732e3ee6d89e312f
zhangbingbing <tosft.sohu.com>
2016-10-12 15:05:06 +0800

    finish.....

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"search"
+	"strconv"
+	"strings"
 	"utils"
 )
 
@@ -11,11 +13,25 @@ func main() {
 	flag.Parse()
 	root := flag.Arg(0)
 	fileNames := utils.GetFileList(root)
+
 	for _, v := range fileNames {
-		fmt.Println(v)
-		lines := utils.ReadFile(v)
-		containStrs := search.SliceContainsSuccess(lines)
-		fmt.Println(len(containStrs))
-		fmt.Println(containStrs[0])
+		size, sum := test(v)
+		fmt.Println(spiltStr(v, size, sum))
 	}
+
+}
+
+func spiltStr(fileName string, size int, sum int) string {
+	splitStrs := strings.Split(fileName, "_")
+	date := strings.Split(splitStrs[1], ".")[0]
+	str := fmt.Sprintf("日期:%s  充值笔数:%s  总金额:%s", date, strconv.Itoa(size), strconv.Itoa(sum))
+	return str
+}
+
+func test(fileName string) (size, sum int) {
+	lines := utils.ReadFile(fileName)
+	containStrs := search.SliceContainsSuccess(lines)
+	sum = search.SumFile(containStrs)
+	size = len(containStrs)
+	return
 }
--- src/search/search.go
+++ src/search/search.go
@@ -1,10 +1,16 @@
 package search
 
 import (
+	"fmt"
+	"log"
+	"strconv"
 	"strings"
 )
 
-const success = "success"
+const (
+	success = "success"
+	price   = "price:"
+)
 
 func SliceContainsSuccess(strs []string) []string {
 	var containStrs []string
@@ -18,4 +24,40 @@ func SliceContainsSuccess(strs []string) []string {
 
 func strContainSuccess(str string) bool {
 	return strings.Contains(str, success)
+}
+
+func strContainPrice(str string) bool {
+	return strings.Contains(str, price)
+}
+
+func SplitStr(str string) []string {
+	return strings.Split(str, " ")
+}
+
+func replacePrice(str string) int {
+	s := strings.Replace(str, price, "", -1)
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
+		log.Println(err)
+	}
+	return num
+}
+
+func Sum(str string) int {
+	var sum int
+	for _, v := range SplitStr(str) {
+		if strContainPrice(v) {
+			sum += replacePrice(v)
+		}
+	}
+	return sum
+}
+
+func SumFile(lines []string) int {
+	var sum int
+	for _, v := range lines {
+		sum += Sum(v)
+	}
+	return sum
 }
